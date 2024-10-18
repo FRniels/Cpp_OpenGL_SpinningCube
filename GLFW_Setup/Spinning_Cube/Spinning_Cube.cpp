23ab7c8310d4054627f521fd1f13be7386dd3c77
FRniels <115066333+FRniels@users.noreply.github.com>
2024-10-18 23:36:48 +0200

    Made a seperate folder for the GLFW setup example

---
+++ GLFW_Setup/Spinning_Cube/Spinning_Cube.cpp
@@ -0,0 +1,47 @@
+// #include <iostream>
+#include "GLFW/glfw3.h"
+
+int main()
+{
+    GLFWwindow* window;
+
+	/*Initialize the library*/
+	if (!glfwInit())
+		return -1;
+
+	/*Create windowed mode window and it's openGl context*/
+	window = glfwCreateWindow(800, 600, "Window", NULL, NULL);
+	if (!window)
+	{
+		glfwTerminate();
+		return -1;
+	}
+
+	/*Set the window context to be the current context*/
+	glfwMakeContextCurrent(window);
+
+	/*Loop until the user closes the window*/
+	while (!glfwWindowShouldClose(window))
+	{
+		/*RENDER*/
+		glClearColor(0.996F, 0.54F, 0.094F, 0.0F); // Orange
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		glBegin(GL_TRIANGLES);
+		glVertex2f(0.0f, 0.5f);
+		glVertex2f(-0.5f, -0.5f);
+		glVertex2f(0.5f, -0.5f);
+		glEnd();
+
+		/*Swap front and back buffers*/
+		glfwSwapBuffers(window);
+
+		/*Poll and process events*/
+		glfwPollEvents();
+
+	}
+
+	glfwTerminate();
+
+    return 0;
+}

79ebcb8ba57a0157952183e479a600850655ed2f
FRniels <115066333+FRniels@users.noreply.github.com>
2024-10-19 22:35:14 +0200

    Added functionality to ParseShader(). The parser can detect if it is parsing the vertex or the fragment shader. The parser can detect and ignore comments in the .shader file that start on a newline and start with //. TO DO: the parser should be able to detect comments starting with // on the same line after a piece of shader code.

--- OpenGL_Shaders_Example/Spinning_Cube/GL_ShaderProgram.cpp
+++ OpenGL_Shaders_Example/Spinning_Cube/GL_ShaderProgram.cpp
@@ -2,6 +2,8 @@
 
 #include <iostream>
 #include <fstream>
+#include <sstream>
+#include <string>
 
 //struct ShaderProgramSource
 //{
@@ -13,9 +15,41 @@ ShaderProgramSource ParseShader(const std::string& filepPath)
 {
 	std::ifstream stream(filepPath);
 
+	/* An enumerable class to set the state/mode to the kind of shader the while loop is parsing.
+	   These types are used as the index for the string stream array (ss[]) */
+	enum class ShaderType
+	{
+		NONE = -1, VERTEX = 0, FRAGMENT = 1
+	};
+
+	std::string line;
+	std::stringstream ss[2];            // An array to store both the vertex and fragment shader sources.
+	ShaderType type = ShaderType::NONE; // Create a shaderType instance to keep track of which shader source is being parsed at a given time.
+
 	if (stream)
 	{
-		std::cout << "File path is okay and stream object is created." << std::endl;
+		// std::cout << "File path is okay and stream object is created." << std::endl;
+
+		while (getline(stream, line))
+		{
+			if (strncmp(line.c_str(), "//", 2) != 0) // Ignore comments starting on a new line that start with a //
+			{
+				if (line.find("#shader") != std::string::npos)
+				{
+					if (line.find("vertex") != std::string::npos)
+					{
+						type = ShaderType::VERTEX;
+						std::cout << "Vertex shader found." << std::endl;
+					}
+					else if (line.find("fragment") != std::string::npos)
+					{
+						type = ShaderType::FRAGMENT;
+						std::cout << "Fragment shader found." << std::endl;
+					}
+				}
+			}
+
+		}
 	}
 	else
 	{

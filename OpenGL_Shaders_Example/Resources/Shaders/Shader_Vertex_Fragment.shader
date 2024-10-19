9d2ea3209749c0c20966b3eb336577590822719f
FRniels <115066333+FRniels@users.noreply.github.com>
2024-10-20 01:32:33 +0200

    The shader parser can now handle block comments inside of a .shader file, this in the form of: /* comment */

--- OpenGL_Shaders_Example/Resources/Shaders/Shader_Vertex_Fragment.shader
+++ OpenGL_Shaders_Example/Resources/Shaders/Shader_Vertex_Fragment.shader
@@ -1,14 +1,26 @@
-// The parser i wrote can handle comments starting with: //
+// This shader parser can handle comments: 
+// 	- Single comments: on new lines and single comments following a line of shader code: //
+//  - Block comments:  on new lines, following and ending on a line of shader code, following a line of shader code and ending on a new line: /* */
 // Note: 
 //	this file is not a .cpp file, we want to use this as a text file and give it the extension .shader
 //	You can also create a seperate file for each shader, but in this case both the vertex and fragment shader are written in 1 file and a parser is written for this file.
 //	In case of seperate files, you could name your file extensions like .vertexShader or .fragmentShader, it really doesn't matter which
 //	extension, but make sure this file is text and not code that is compiled like .cpp
 //	#shader vertex/fragment is used in this parser to identify which shader is which.
+
+/* block comment */
+
+/* block
+   comment */
+   
+/*
+	block comment
+*/
+
 #shader vertex
 #version 330 core
 
-layout(location = 0) in vec4 position;
+layout(location = 0) in vec4 position; /* A block comment on the same line as shader source code */
 
 void main()      // A comment on the same line as shader source code
 {
@@ -18,7 +30,9 @@ void main()      // A comment on the same line as shader source code
 #shader fragment // Another comment that the shader parser will ignore
 #version 330 core
 
-out vec4 color;
+out vec4 color; /* A
+				   weird
+				   block comment */
 
 void main()
 {
--- OpenGL_Shaders_Example/Spinning_Cube/GL_ShaderProgram.cpp
+++ OpenGL_Shaders_Example/Spinning_Cube/GL_ShaderProgram.cpp
@@ -23,7 +23,7 @@ ShaderProgramSource ParseShader(const std::string& filepPath)
 	};
 
 	std::string line;
-	std::stringstream ss[2];            // An array to store both the vertex and fragment shader sources.
+	std::stringstream ss[2];            // An stream array to store both the vertex and fragment shader sources.
 	ShaderType type = ShaderType::NONE; // Create a shaderType instance to keep track of which shader source is being parsed at a given time.
 
 	if (stream)
@@ -32,11 +32,23 @@ ShaderProgramSource ParseShader(const std::string& filepPath)
 
 		while (getline(stream, line))
 		{
-			// TO DO: Block comment support /**/
-			if (strncmp(line.c_str(), "//", 2) != 0) // Ignore comments starting on a new line that start with a //
+			if (strncmp(line.c_str(), "/*", 2) == 0)           // Start of a block comment on a new line starting with /*
 			{
-				// Check which kind of shader is following in the upcoming lines
-				if (line.find("#shader") != std::string::npos)
+				do
+				{
+					if (line.find("*/") != std::string::npos)  // End of the block comment
+					{
+						break;
+					}
+				} while (getline(stream, line));
+				// THERE IS NO CHECK TO SEE IF A BLOCK COMMENT OPENED WITH /* BUT NOT CLOSED WITH */ => ALL THE LINES AFTER /* WILL BE IGNORED.
+				// Go to the next line, when the loop breaks due to finding the end of the block comment. 
+				// A comment like this could be made: /*comment*/ // comment => the last comment would be automatically skipped so this is not a problem.
+				// This does not allow for comments inside statements like if(i == 0 /* comment*/){} !
+			}
+			else if (strncmp(line.c_str(), "//", 2) != 0)      // Start of a comment a new line starting with //
+			{
+				if (line.find("#shader") != std::string::npos) // Check which kind of shader is following in the upcoming lines
 				{
 					if (line.find("vertex") != std::string::npos)
 					{
@@ -52,11 +64,30 @@ ShaderProgramSource ParseShader(const std::string& filepPath)
 				else // Actual shader code is found
 				{
 					std::size_t comment_start_index = std::string::npos;
-					comment_start_index = line.find("//");               // Check if a comment is made after a line of shader code
 					
-					if (comment_start_index != std::string::npos)        // Remove the comment after the line of shader code
+					if ((comment_start_index = line.find("//")) != std::string::npos)      // Remove a comment starting with // after a line of shader code
 					{
 						line = line.substr(0, (comment_start_index)); 
+					}
+					else if ((comment_start_index = line.find("/*")) != std::string::npos) // Remove a block comment starting with /* after a line of shader code
+					{
+						if (line.find("*/") != std::string::npos)                          // Check if the block comment is ending on the same line
+						{
+							line = line.substr(0, (comment_start_index));
+						}
+						else                                                               // Else check the next lines until reaching the end of the block comment, ending with */
+						{
+							line = line.substr(0, (comment_start_index));
+							std::cout << line << std::endl;                                // Save the valid shader source code before the block comment starts with /*
+							while (getline(stream, line))
+							{
+								if (line.find("*/") != std::string::npos)                  // No checking if the end of a block comment */ eventually exist, if not, all lines after the opening /* will be ignored.
+								{
+									line = "";
+									break;
+								}
+							}
+						}
 					}
 
 					std::cout << line << std::endl;

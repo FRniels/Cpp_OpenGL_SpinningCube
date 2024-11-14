3ccf8a4d7ca3a60607579838fd3af269f95581e3
FRniels <115066333+FRniels@users.noreply.github.com>
2024-11-14 01:26:16 +0100

    Abstracting the glfw calls into a Window class. To do: The window resize callback needs to be coupled with the Window class.

--- Spinning_Cube/Spinning_Cube/Spinning_Cube.cpp
+++ Spinning_Cube/Spinning_Cube/Spinning_Cube.cpp
@@ -7,6 +7,7 @@
 #include "GL/glew.h"     // This glew header contains the function pointers to OpenGl functions
 #include "GLFW/glfw3.h"
 
+#include "Window.h"
 #include "GL_User_Types.h"
 #include "GL_ErrorHandeling.h"
 #include "GL_ShaderProgram.h"
@@ -18,7 +19,7 @@
 
 
 void Callback_Resize(GLFWwindow* window, int width, int height);
-void GL_Render(void);
+void Render(void);
 
 
 vec2i u_window_coo = { 0, 0 }; // Global so that the window resize callback has access to this vec2f => TO DO: CAN I PASS AN EXTRA ARGUMENT TO THE CALLBACK ??
@@ -27,21 +28,18 @@ unsigned int shader_program;
 
 int main()
 {
-	GLFWwindow* window;
+	Window window(800, 600, "Title");
 
-	if (!glfwInit())										       // Initialize the library 
-		return -1;
-
-	window = glfwCreateWindow(800, 600, "Window", NULL, NULL);     // Create windowed mode window and it's openGl context 
-	if (!window)
+	if (glewInit() != GLEW_OK)
 	{
-		glfwTerminate();
-		return -1;
+		std::cout << "GLEW failed to init!" << std::endl;
+		window.Exit();
 	}
 
-	glfwMakeContextCurrent(window);                                 // Set the window context to be the current context 
 
-	glfwSetWindowSizeCallback(window, Callback_Resize);
+	glfwSetWindowSizeCallback(window.GetWindow(), Callback_Resize);
+
+
 
 	if (glewInit() != GLEW_OK)                                      // Initialize GLEW: YOU FIRST NEED TO HAVE A VALID OPENGL CONTEXT!!! SO CALL THIS AFTER THE CONTEXT CREATION 
 		std::cout << "Error with initializing GLEW!" << std::endl;
@@ -68,7 +66,6 @@ int main()
 	vertex_array.AddBuffer(bufferV, layout_bufferV);
 	GL_ElementBuffer bufferE(indices, 2 * 3 * sizeof(unsigned int)); // The element buffer is bound to the OpenGL contect on instantiation
 
-
 	shader_program = CreateShaderProgram("../Resources/Shaders/Shader_Vertex_Fragment.shader"); // Create Shader Program 
 	UseShaderProgram(shader_program);
 
@@ -82,17 +79,13 @@ int main()
 	vec4f color_vec = { 0.0f, 1.0f, 1.0f, 1.0f };
 	// SetUniform(shader_program, "u_Color",        color_vec);       // Note: vec4 will be passed as pointer as it is an array => Not using this uniform in the shader anymore
 	
-	glfwGetWindowSize(window, NULL, &u_window_coo[1]);
+	glfwGetWindowSize(window.GetWindow(), NULL, &u_window_coo[1]);
 	GL_Uniform u_window_height = GetUniform(shader_program, "uWindow_Height");
 	SetUniform1f(shader_program, u_window_height.Get_Handle(), u_window_coo[1]);
 
 	// IMPORTANT: Always unbind the vao before unbinding the associated vertex/element buffer. If the vertex/element buffer is unbound before 
 	//            the vao is unbound, the vertex/element will be unbound from the vao, thus the vao will not have the vertex/element buffer bound to it anymore.
 	//            When trying to draw with such an 'unconfigured' vao, null pointer errors can/will or even worse, undefinded behaviour will occur.
-	// 
-	// Unbind all buffers including the vao and the shader program.
-	// When this specific vao is needed when drawing, this vao can be bound to the OpenGL context at that moment.
-	// The shader program is not stored inside the vao so it needs to be bound to the OpenGL context seperately.
 	GL_Vertex_Array::Unbind();
 	GL_VertexBuffer::Unbind();
 	GL_ElementBuffer::Unbind();
@@ -100,17 +93,17 @@ int main()
 
 
 	// RENDER LOOP 
-	while (!glfwWindowShouldClose(window))                       // Loop until the user closes the window
+	while (!glfwWindowShouldClose(window.GetWindow()))                       // Loop until the user closes the window
 	{
 		// Bind the required/necesarry/application specific vao and shader program to the OpenGL context before drawing. => In this case, the vao and shader program stay the same thus is would be unnecessarry to perform these gl calls each iteration.
 		vertex_array.Bind();
-		GL_Call(glUseProgram(shader_program));					 // Bind the required shader program to th OpenGL context
+		GL_Call(glUseProgram(shader_program));			// Bind the required shader program to th OpenGL context
 
-		GL_Render();											 // Render the scene
+		Render();									    // Render the scene
 
-		glfwSwapBuffers(window);                                 // Swap front and back buffers 
+		window.SwapBuffers();
 
-		glfwPollEvents();                                        // Poll and process events
+		window.PollEvents();
 	}
 
 	// Clean-up:
@@ -119,7 +112,7 @@ int main()
 	vertex_array.Delete();
 	GL_Call(glDeleteProgram(shader_program));
 
-	glfwTerminate();
+	window.Exit();
 
     return 0;
 }
@@ -133,7 +126,7 @@ void Callback_Resize(GLFWwindow* window, int width, int height)
 	SetUniform1f(shader_program, "uWindow_Height", height); // TO DO: Save the handle the first time and use this handle to search for all following calls to SetUniform() to avoid the overhead of search by string name
 }
 
-void GL_Render(void)
+void Render(void)
 {
 	vec4f clear_color = { 0.996F, 0.54F, 0.094F, 0.0F };
 	GL_ClearScreen(clear_color);
